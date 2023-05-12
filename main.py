57d0cd7a02257131b0c77c1c0133f2ff84192a81
Ruslan Akhmetov <110201517+ra83205@users.noreply.github.com>
2023-05-12 23:25:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ class Message(BaseModel):
 
 @app.post("/ask")
 async def ask(request: Request, message: Message) -> dict:
-    auth_key = os.getenv('AUTH_KEY')
-    if not auth_key:
-        raise HTTPException(status_code=401, detail='Authorization key is missing')
-
-    if auth_key != request.headers.get('Authorization'):
+    # Get the user-defined auth key from the environment variables
+    user_auth_key = os.getenv('USER_AUTH_KEY')
+    
+    # Check if the user has defined an auth key,
+    # If so, check if the auth key in the header matches it.
+    if user_auth_key and user_auth_key != request.headers.get('Authorization'):
         raise HTTPException(status_code=401, detail='Invalid authorization key')
 
+    # Execute your code without authenticating the resource
     chatbot = Chatbot(message.session_id)
     response = chatbot.ask(message.message)
     return response
